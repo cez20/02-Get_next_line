fdc7e23644b0ea7890d6af43b7ccedd9118f2aea
Cesar Menjivar <cemenjiv@student.42quebec.com>
2021-10-26 16:41:39 -0400

    Exploring open() and write() system call

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,21 @@
 /*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 13:31:04 by cemenjiv          #+#    #+#             */
-/*   Updated: 2021/10/22 13:31:31 by cemenjiv         ###   ########.fr       */
+/*   Updated: 2021/10/26 11:35:59 by cemenjiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_next_line(int fd)
+/*
+	1- Calling GNL in loop will allow to read text available of FD oneline at a time until end of it
+	2 -Make sure file GNL behave well when read from a FILE, and when it reads from STANDARD INPUT
+	3 -Program must compile with flag gcc -Wall -Wextra -Werror -D BUFFER_SIZE = 42 <files> .c
+	4 -Use READ, MALLOC AND FREE,
+	5 -Your READ must use the BUFFER SIZE defined during compilation to read from file or from stdin.
+*/
+
+/*char	*get_next_line(int fd)
 {
 
 
-}
+}*/
+
---
+++ get_next_line.h
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/24 11:49:03 by cemenjiv          #+#    #+#             */
+/*   Updated: 2021/10/24 11:50:48 by cemenjiv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*This file must contain AT LEAST the prototype of the function get_next_line. In other words
+it may contain other prototype*/
+char	*get_next_line(int fd);
